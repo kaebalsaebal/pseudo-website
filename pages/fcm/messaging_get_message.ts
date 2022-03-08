b2ef28713a6eccfaece6d1aa26edda47bf7c2c0b
YJSNPIDISK <56146987+YJSNPIDISK@users.noreply.github.com>
2022-03-08 16:04:24 +0900

    fcm 안백그라운드 알림 및 프론트에서 fcm요청 추가

--- pages/fcm/messaging_get_message.ts
+++ pages/fcm/messaging_get_message.ts
@@ -4,7 +4,9 @@ import { app } from '../../firebase/firebaseConfig';
 const initMessage = () => {
 	const messaging = getMessaging(app);
 	onMessage(messaging, (payload) => {
-		console.log(payload);
+		console.log(payload.notification.title);
+		console.log(payload.notification.body);
+		alert(payload.notification.body + '이다');
 	});
 };
 
